7cd8d8ac6a16d3b30138b7ee98912f23f1169dbd
Huseyn941 <160209106+Huseyn941@users.noreply.github.com>
2024-03-08 22:54:37 +0400

    Update Task_9.py

--- Task_9.py
+++ Task_9.py
@@ -3,6 +3,11 @@ def main():
   Kodunuzu buraya yazin.
   '''
   pass
+  n = int(input("Number of loaves: "))
+  pr = 3.49
+  print(f'Total price: {n*pr:.3f} dollars')
+  print(f'Discount: {n*pr*0.6:.2f} dollars')
+  
 
 if __name__ == "__main__":
   main()
