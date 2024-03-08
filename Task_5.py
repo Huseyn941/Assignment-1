682bfd851da4e833336e07e8e03e7df5e6137977
Huseyn941 <160209106+Huseyn941@users.noreply.github.com>
2024-03-08 22:49:58 +0400

    Update Task_5.py

--- Task_5.py
+++ Task_5.py
@@ -5,7 +5,16 @@ def main():
   '''
   Kodunuzu buraya yazin.
   '''
-  pass
+  import math
+a=int(input("a: "))
+b=int(input("b: "))
+print(f'{a} + {b} = {a+b}')
+print(f'{a} - {b} = {a-b}')
+print(f'{a} * {b} = {a*b}')
+print(f'{a} % {b} = {a%b}')
+print(f'{a} // {b} = {a//b}')
+print(f'{a} ** {b} = {a**b}')
+print(f'log10({a}) = {math.ln({a})}') 
 
 if __name__ == "__main__":
   main()
