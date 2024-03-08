e5f117bb0990be6ec099d552432a820499174a4e
Huseyn941 <160209106+Huseyn941@users.noreply.github.com>
2024-03-08 22:48:29 +0400

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -2,7 +2,16 @@ def main():
   '''
   Kodunuzu buraya yazin.
   '''
-  pass
+  reqem = int(input('Enter 4 digit number: '))
+a = reqem//1000
+b = reqem//100%10
+c = reqem//10%10
+d = reqem%10
+cem = a+b+c+d
+hasil = a*b*c*d
+print(f'Digits: {a} {b} {c} {d}')
+print(f'Sum of digits: {a}+{b}+{c}+{d}={cem}')
+print(f'Product of digits: {a}*{b}*{c}*{d}={hasil}')
 
 if __name__ == "__main__":
   main()
