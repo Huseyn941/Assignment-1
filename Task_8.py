51cea6cbe48b525c225fe3194fd63f1fd9fb894f
Huseyn941 <160209106+Huseyn941@users.noreply.github.com>
2024-03-08 22:53:44 +0400

    Update Task_8.py

--- Task_8.py
+++ Task_8.py
@@ -2,7 +2,9 @@ def main():
   '''
   Kodunuzu buraya yazin.
   '''
-  pass
+  C = float(input("Temperatur(C):"))
+  F = C*(9/5)+32
+  print(f'{C:.0f} Celcius = {F:.2f} Farenheit')
 
 if __name__ == "__main__":
   main()
