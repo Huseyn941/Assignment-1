b4a0a1f33a8d179bf1374b72df3d021aafebf9b0
Huseyn941 <160209106+Huseyn941@users.noreply.github.com>
2024-03-08 22:46:57 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -2,7 +2,11 @@ def main():
   '''
   Kodunuzu buraya yazin.
   '''
-  pass
+  a = float(input("Notebook price: "))
+b = float(input("Pencil price: "))
+c = float(input("Number of notebooks: "))
+d = float(input("Number of pencils: "))
+print(c,"notebooks and ",d,"pencils total price:",c*a+b*d,"azn")
 
 if __name__ == "__main__":
   main()
