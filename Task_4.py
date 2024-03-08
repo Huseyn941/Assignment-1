0bc01070f7d5fe9e7e754c7e5fcdcdf19aabc9f6
Huseyn941 <160209106+Huseyn941@users.noreply.github.com>
2024-03-08 22:49:14 +0400

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -2,7 +2,13 @@ def main():
   '''
   Kodunuzu buraya yazin.
   '''
-  pass
+  import math
+sd1 = float(input("Side 1: "))
+sd2 = float(input("Side 2: "))
+sd3 = float(input("Side 3: "))
+s = (sd1+sd2+sd3)/2
+area = math.sqrt(s*(s-sd1)*(s-sd2)*(s-sd3))
+print(f'Area of triangle: {area:.2f}')
 
 if __name__ == "__main__":
   main()
